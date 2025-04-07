7686c093cce59048bc80cd626fbeb3ae8ad3c234
Jasper Liauw <jasperliauw@gmail.com>
2025-04-07 18:35:23 +0800

    Created filelocation and check if the file exists or not

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
-print("Hello World!")
+######################
+#Imports
+
+import os
+
+#######################
+#Global Variables
+
+fileSelected = False
+fileName = ""
+
+#######################
+
+
+print("Welcome to Lesson Substitution")
+
+
+
+def checkFileExistence(fileName):
+    if os.path.exists(fileName):
+        return True
+    else:
+        print("File does not exist, please try again")
+        return False
+
+
+
+while not fileSelected:
+    fileLocation = input("If the file is in the same folder as the program, please input (1)\nIf the file is not in the same folder as the program, please input (2)\n")
+    if fileLocation == "1":
+        fileName = input("Please input the name of the file: ")
+        fileSelected = checkFileExistence(fileName)
+    elif fileLocation == "2":
+        fileName = input("Please input the FULL DIRECTORY of the file: ")
+        fileSelected = checkFileExistence(fileName)
+
+    else:
+        print("Invalid option, please try again")
+
+
+
