22ff1d9192c3364b7137aad3e7614cbbee4c4b29
Jasper Liauw <jasperliauw@gmail.com>
2025-04-10 17:25:52 +0800

    finished SC.txt part, onto timetable

--- main.py
+++ main.py
@@ -8,26 +8,78 @@ import os
 
 fileSelected = False
 fileNameSC = ""
-schoolCode = []
-lessonVar = []
+fileNameTimetable = ""
+schoolCode = ""
+schoolCodeID = 0
+foundSchoolCode = False
+schoolName = ""
 lessonDays = 0
 lessonAmount = 0
-
-
 
 #######################
 
 
-print("Welcome to Lesson Substitution")
+
 
 
 
 def checkFileExistence(fileName):
-    if os.path.exists(fileName):
+    if os.path.isfile(fileName):
         return True
     else:
         print("File does not exist, please try again")
         return False
+
+
+
+def checkTimetableValidity():
+    print(fileNameTimetable)
+
+
+
+
+def schoolDetails():
+    schoolName = splitData[schoolCodeID][1]
+    lessonDays = int(splitData[schoolCodeID][2])
+    lessonAmount = int(splitData[schoolCodeID][3])
+    global fileNameTimetable
+    print("Details:\nSchool Name:", schoolName, "\nAmount of lessons in a day:", lessonDays, "\nAmount of lessons:", lessonAmount)
+    isCorrect = input("Would you like to proceed? (y/n) ")
+    if isCorrect.upper() == "Y":
+        fileNameTimetable = input("Please input the full directory of timetable")
+        checkTimetableValidity()
+    elif isCorrect.upper() == "N":
+        isRetry = input("Would you like to retry? (y/n)")
+        if isRetry.upper() == "Y":
+            schoolCodeCheck()
+        elif isRetry.upper() == "N":
+            print("Thank You for using this program, goodbye!")
+        else:
+            print("Please try again")
+            schoolDetails()
+    else:
+        print("Please try again")
+        schoolDetails()
+
+
+
+def schoolCodeCheck():
+    userInputSchoolCode = input("Please enter your school code\n")
+    foundSchoolCode = False
+    global schoolCode
+    global schoolCodeID
+    for i in range(len(splitData)):
+        if userInputSchoolCode == splitData[i][0]:
+            schoolCode = splitData[i][0]
+            schoolCodeID = i
+            foundSchoolCode = True
+    if foundSchoolCode:
+        print("School code found.")
+        print(schoolCode)
+        schoolDetails()
+    else:
+        print("School code not found, please try again")
+        schoolCodeCheck()
 
 
 
@@ -36,9 +88,10 @@ while not fileSelected:
     if checkFileExistence(fileNameSC):
         fileSelected = True
 
-
-
 file = open(fileNameSC, mode='r')
 fileLines = file.read()
 lines = fileLines.splitlines()
 splitData = [line.split(';') for line in lines]
+schoolCodeCheck()
+
+
